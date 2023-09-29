778ad10365c8f2f07fb6401819926227f8680b5b
Bellam Rahul <142339913+rahul-bellam@users.noreply.github.com>
2023-09-29 23:46:32 +0530

    assignment6q6.c

---
+++ assignment6q6.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->prev = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void reverseDoublyLinkedList(struct Node** head) {
+    struct Node* temp = NULL;
+    struct Node* current = *head;
+
+    while (current != NULL) {
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        current = current->prev;
+    }
+
+    if (temp != NULL) {
+        *head = temp->prev;
+    }
+}
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        printf("%d <-> ", node->data);
+        node = node->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->prev = head;
+    head->next->next = createNode(3);
+    head->next->next->prev = head->next;
+    head->next->next->next = createNode(4);
+    head->next->next->next->prev = head->next->next;
+    head->next->next->next->next = createNode(5);
+    head->next->next->next->next->prev = head->next->next->next;
+
+
+    printf("Original Doubly Linked List: ");
+    printList(head);
+
+    reverseDoublyLinkedList(&head);
+
+    printf("Reversed Doubly Linked List: ");
+    printList(head);
+
+    return 0;
+}
